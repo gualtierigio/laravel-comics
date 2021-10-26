63c2803ad2f7e1cbec92acfa9711e388ca98968d
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-26 18:12:31 +0200

    aggiunta img e descrizione a comic

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -5,9 +5,15 @@
 <section class="d-flex" id="comic_wrapper">
 
 {{-- sezione descrittiva --}}
+    <img class="cover_comic" src="{{ $comic['thumb'] }}" alt="">
+
     <div class="description">
             
         <h2>{{ $comic['title'] }}</h2>
+        <div>
+            <p>{{ $comic['price'] }}</p>
+        </div>
+        <p>{{ $comic['description'] }}</p>
 
     </div>
 
