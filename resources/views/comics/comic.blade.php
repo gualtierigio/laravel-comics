1455ceec29ba20a34e1187123e495d10437c7b10
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-11-02 17:04:52 +0100

    sistemato cards

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -3,24 +3,34 @@
 @section('content')
 
 <section class="d-flex" id="comic_wrapper">
-
 {{-- sezione descrittiva --}}
-    <img class="cover_comic" src="{{ $comic['thumb'] }}" alt="img thumb">
+    <div class="description-wrapper">
+        <img class="cover_comic" src="{{ $comic['thumb'] }}" alt="img thumb">
+        <div class="description">   
+            <h2>{{ $comic['title'] }}</h2>
 
-    <div class="description">
-            
-        <h2>{{ $comic['title'] }}</h2>
-        <div>
-            <p>{{ $comic['price'] }}</p>
+            <div class="tab-wrapper d-flex justify-content-between">
+                <div class="price-wrap d-flex justify-content-between align-items-center">
+                    <p>U.S Price: {{ $comic['price'] }}</p>
+                    <p>AVALIABLE</p>
+                </div>
+                <div class="availability">
+                    <p>Check Availability</p>
+                </div>
+            </div>
+
+            <div>
+                <p>{{ $comic['description'] }}</p>
+            </div>
         </div>
-        <p>{{ $comic['description'] }}</p>
-
     </div>
-
 {{-- immagine apply now --}}
-    <figure>
-        <img src="{{ asset('images/adv.jpg') }}" alt="">
-    </figure>
+    <div class="advert-superman">
+        <h2>advertisement</h2>
+        <figure>
+            <img src="{{ asset('images/adv.jpg') }}" alt="">
+        </figure>
+    </div>
 
 </section>
     
