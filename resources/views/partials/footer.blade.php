2f2dda76aa547f107a2f703f8a4d5b9310bc3405
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-11-02 12:38:57 +0100

    links footer ok

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,62 @@
 <footer>
 
     <div class="page-wrap d-flex">
-       <ul>
-          <li><h3>title</h3></li>
-         @foreach($links as $link)
-          <li><a href="{{ $link['route'] }}">{{ $link['text'] }}</a></li>
-         @endforeach
-       </ul>
+
+      <div>
+         <ul>
+            <li><h3>dc comics</h3></li>
+           @foreach($links as $link)
+            <li><a href="{{ $link['route'] }}">{{ $link['text'] }}</a></li>
+           @endforeach
+
+           <li><h3>dc comics</h3></li>
+           <li><a href="#">Shop DC</a></li>
+           <li><a href="#">Shop DC Collectibles</a></li>
+         </ul>
+
+         <ul>
+
+           <li><h3>dc</h3></li>
+           <li>
+              <a href="#">Terms of use</a>
+            </li>
+           <li>
+              <a href="#">Privacy policy (New)</a>
+            </li>
+           <li>
+              <a href="#">Ad Choices</a>
+            </li>
+           <li>
+              <a href="#">Advertising</a>
+            </li>
+           <li>
+              <a href="#">Jobs</a>
+            </li>
+           <li>
+              <a href="#">Subscription</a>
+            </li>
+           <li>
+              <a href="#">Talent Workshops</a>
+            </li>
+           <li>
+              <a href="#">CPSC Certificates</a>
+            </li>
+           <li>
+              <a href="#">Ratings</a>
+            </li>
+           <li>
+              <a href="#">Shop Help</a>
+            </li>
+           <li>
+              <a href="#">Contact Us</a>
+            </li>
+
+         </ul>
+
+      </div>
 
        <div>
-         <img src="{{ asset('images/dc-logo-bg.png')}}" alt="">
+         <img src="{{ asset('images/dc-logo-bg.png')}}" alt="dc-logo-bg">
       </div>
     </div>
 
